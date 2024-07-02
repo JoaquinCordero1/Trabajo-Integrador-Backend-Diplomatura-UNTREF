dde57d8ae20891a49d0c993b828073180f2baf07
Joaquin Cordero <joaquincordero751@gmail.com>
2024-07-02 17:28:43 -0300

    C:/Program Files/Git/productos filtra por categoria y nombre

--- app.js
+++ app.js
@@ -15,47 +15,66 @@ app.get("/", (req, res) => {
 // con /productos accedemos a toda la lista
 
 app.get("/productos", (req, res) => {
-  const { categoria } = req.query;
+  const { categoria, nombre } = req.query;
+
+  let filter = {};
+
+  if (categoria) {
+    filter = !categoria
+      ? {}
+      : {
+          categoria: { $regex: `^${categoria.slice(0, 3)}`, $options: "i" },
+        };
+  }
+  if (nombre) {
+    filter = !nombre
+      ? {}
+      : { nombre: { $regex: `^${nombre.slice(0, 3)}`, $options: "i" } };
+  }
 
   // Filtrar las categorias discriminando las mayusculas y con coincidencias en la palabra
-  const filter = !categoria
-    ? {}
-    : { categoria: { $regex: `^${categoria.slice(0, 3)}`, $options: "i" } }; // Buscamos las coincidencias con las tres primeras letras
+  // const filtro = !categoria
+  //   ? {}
+  //   : { categoria: { $regex: `^${categoria.slice(0, 3)}`, $options: "i" } }; // Buscamos las coincidencias con las tres primeras letras
 
   Computer.find(filter)
     .then((computacion) => {
-      if (categoria && computacion.length === 0) {
-        // Si no encuentra productos devuelve el error o si categoria está vacio devuelve todos los productos
+      if ((categoria || nombre) && computacion.length === 0) {
+        // Si no encuentra productos devuelve el error o si su categoria está vacia devuelve todos los productos
         return res
           .status(404)
-          .send("No se encontraron productos para la categoria especificada");
+          .send(`La busqueda: ${nombre || categoria} no se encontró`);
       }
       res.json(computacion);
     })
     .catch((error) => {
       console.log("Error al encontrar productos: ", error);
-      res.status(500).send("Hubo un problema para encontrar los productos");
+      res.status(500).send("Hubo un problema para encontrar los productos.");
     });
 });
 
+// Filtrar producto por su id (codigo)
 app.get("/productos/:id", (req, res) => {
   const { id } = req.params;
-  const query = { codigo: id };
-  Computer.find(query)
+  const findCode = { codigo: id };
+
+  Computer.find(findCode)
     .then((productID) => {
-      if (productID) {
-        res.json(productID);
-      } else {
+      if (findCode && productID.length === 0) {
+        console.log(
+          "Aún no hay productos con ese código, prueba utilizando otro"
+        );
         res
           .status(404)
-          .send(
-            `El producto con el id ${id} no pudo ser econtrado o no existe`
-          );
+          .send("El producto no se encontró. Prueba buscando otro. ");
+      } else {
+        console.log("Búsqueda exitosa");
+        res.json(productID);
       }
     })
     .catch((error) => {
-      console.log("Error al obtener la pelicula", error);
-      res.status(500).send(`Error al encontrar el producto`);
+      console.log("Error al obtener el producto: ", error);
+      res.status(500).send("Error al encontrar el producto.");
     });
 });
 
